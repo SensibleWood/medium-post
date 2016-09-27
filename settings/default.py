587006e9e64ac70ea6ece11a52f0f08198de0e10
Chris Wood <chris@arborcomputing.com>
2016-09-27 15:11:25 +0100

    Corrected token endpoint

--- settings/default.py
+++ settings/default.py
@@ -7,7 +7,7 @@ SECRET_KEY = uuid4().hex
 MEDIUM_CLIENT_ID = environ['MEDIUM_CLIENT_ID']
 MEDIUM_CLIENT_SECRET = environ['MEDIUM_CLIENT_SECRET']
 MEDIUM_REDIRECT_URI = environ['MEDIUM_REDIRECT_URI']
-
 MEDIUM_SCOPE = 'basicProfile,publishPost'
+
 MEDIUM_AUTHORIZATION_URL = 'https://medium.com/m/oauth/authorize'
-MEDIUM_TOKEN_URL = '/v1/tokens'
+MEDIUM_TOKEN_URL = 'https://api.medium.com/v1/tokens'
